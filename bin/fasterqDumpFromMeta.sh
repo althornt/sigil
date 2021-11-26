dbf480a6c626e9257561eaaba9251cac74402124
althornt <alxs412@gmail.com>
2021-11-26 01:30:42 +0000

    add options and compression to fastq download script

---
+++ bin/fasterqDumpFromMeta.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+metadata_run_csv="$1"
+out_dir="$2"
+threads="$3"
+
+
+# Make output directory
+mkdir -p "$out_dir"
+cd "$out_dir"
+
+# Extract SRAs from first column and download each fastq
+for sra in $(cut -d, -f1 "$metadata_run_csv" | tail -n +2); do
+
+  echo "$sra"
+
+  # Download fastq
+  cmd="sudo docker run -t --rm \
+      -v $PWD:/mnt/sigil:rw \
+      -w /mnt/sigil \
+      ncbi/sra-tools fasterq-dump -e "$threads" -p "$sra""
+
+  echo "$cmd"
+  $cmd
+
+done
+
+# gzip all downloaded fastq (automatically will not gzip .gz files); pigz is faster than gzip
+echo "Compressing files..."
+pigz *
