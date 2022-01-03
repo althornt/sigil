ad896d9ab46518a795dca02e7b01d6f1ddc4bfc3
althornt <alxs412@gmail.com>
2022-01-03 22:06:16 +0000

    add read in of mesa counts and metadataa and merge

--- bin/combineMESAbatchcorr.R
+++ bin/combineMESAbatchcorr.R
@@ -12,6 +12,25 @@ library(ensembldb)
 library(DESeq2)
 library(pheatmap)
 library(limma)
+library(purrr)
+
+importMetaMESA <- function(row){
+  # Read metadata and add column for run
+  df_metadata <- read.csv(file.path(row[3])) %>%
+    dplyr::select(Run, sigil_general, LM22)
+
+  # Add metadata to column
+  df_metadata$data_source <- row[1] # add name of data source
+  df_metadata$type <- row[4] # add rna-seq type (paired vs single)
+
+  # Get paths to MESA inclusion count files
+  res_path <- file.path(row[2], "mesa_out", "mesa_inclusionCounts.tsv")
+
+  return(list(
+      "ls_kallsto_paths"=res_path,
+      "metadata"=df_metadata,
+      "ls_samples_run"=df_metadata$Run))
+}
 
 ###################
 # MAIN
@@ -45,17 +64,52 @@ if (!dir.exists(file.path(opt$out_dir,"/UMAPs_post_batch_correction/"))){
   dir.create(file.path(opt$out_dir,"/UMAPs_post_batch_correction/"),
               recursive = TRUE, showWarnings = TRUE)}
 
-# Open files
+# Open manifest
 df_manifest <- read.csv(file = opt$manifest, sep = "\t", header=TRUE)
 print(head(df_manifest))
 
+# Import and combine source metadata files
+ls_mesa_meta = apply(df_manifest, 1, importMetaMESA)
+
+# Split into kallisto and metadata files for each data set
+ls_mesa_files <- ls_meta <- ls_sample_names <- c()
+for (item in ls_mesa_meta) {
+     ls_mesa_files <- append(ls_mesa_files, item[1])
+     ls_meta <- append(ls_meta, item[2])
+     ls_sample_names <- append(ls_sample_names, item[3])
+   }
+
+# Merge metadata from each data source by rows
+df_merged_metadata <- do.call("rbind", ls_meta)
+rownames(df_merged_metadata) <- c()
+
+# List of samples with LM22 labels
+ls_smpls_lm22 <- df_merged_metadata %>%
+   dplyr::filter(LM22 != "") %>%
+   dplyr::pull(Run)
+
+# Remove samples without LM22 labels from metadata
+df_merged_metadata_lm22 <- df_merged_metadata %>%
+   dplyr::filter(LM22 != "")
+write.csv(df_merged_metadata_lm22,
+            file.path(file.path(opt$out_dir,"metadata.csv")),
+            row.names = FALSE)
+
+# Merge mesa inclusion count files
+df_mesa_merge <- unlist(ls_mesa_files) %>%
+  lapply(read.csv, sep = "\t") %>%
+  reduce(inner_join, by = "cluster")
+
+# Drop non LM22 samples from mesa counts
+df_mesa_merge_lm22 <- df_mesa_merge[ls_smpls_lm22]
+head(df_mesa_merge_lm22)
+
+# head(df_mesa_merge_lm22[,-1])
 
 
-
-#log2trans_dat <- as.data.frame(log2(dat +1))
-
-
-# Log log transform
+# Log2 transform
+# log2trans_dat <- as.data.frame(log2(df_mesa_merge_lm22 +1))
+# head(log2trans_dat)
 
 
 # Batch correct
