c9a3c4c9c1189f4b3be0c8ec46b9aaf3c28e395e
althornt <alxs412@gmail.com>
2022-03-18 15:46:25 +0000

    add dopar for deseq2 comparisons

--- bin/combineGeneDE.R
+++ bin/combineGeneDE.R
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
-
 library(optparse)
 library(EnsDb.Hsapiens.v86)
 library(tximport)
@@ -12,6 +11,10 @@ library(ensembldb)
 library(DESeq2)
 library(pheatmap)
 library(limma)
+library(foreach)
+library(doParallel)
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
 
 ##################
 # Functions
@@ -20,7 +23,7 @@ library(limma)
 importMetaKallisto <- function(row){
   # Read metadata and add column for run
   df_metadata <- read.csv(file.path(row[3])) %>%
-    dplyr::select(Run, sigil_general, LM22)
+    dplyr::select(Run, sigil_general, LM22, LM6)
 
   # Add metadata to column
   df_metadata$data_source <- row[1] # add name of data source
@@ -81,6 +84,18 @@ runDE_1_vs_all <- function(meta_col_to_use, cell_type_val) {
 
   print(head(sampleTable))
 
+  # add input of outdir here, whiich will be diff for LM6 and LM22
+  ls_DE_up_down <- runDEseq2(sampleTable,meta_col_to_use, cell_type_val)
+  print(ls_DE_up_down)
+  print("runDE 1 vs all func done")
+  return(ls_DE_up_down)
+}
+
+runDEseq2 <- function(sampleTable,meta_col_to_use, cell_type_val){
+
+  print("_____________in runDE function")
+  print(head(sampleTable))
+
   # Run DESEQ2 including the batch effect variables - data_source and sequencing type
   dds <- DESeqDataSetFromTximport(txi = txi.kallisto,
                                   colData = sampleTable,
@@ -95,8 +110,15 @@ runDE_1_vs_all <- function(meta_col_to_use, cell_type_val) {
   res<-results(dds_)
   res<-res[order(res$padj),]
 
+  path_to_deseq2_folder <- paste0(opt$out_dir,"/compare_",meta_col_to_use,"/deseq2_outputs/")
+  print(path_to_deseq2_folder)
+
+  if (!dir.exists(file.path(path_to_deseq2_folder))){
+    dir.create(file.path(path_to_deseq2_folder),recursive = TRUE, showWarnings = TRUE)
+    }
   #Write output to file
-  write.csv(as.data.frame(res),file.path(opt$out_dir,"deseq2_outputs",paste0(cell_type_val,".csv")))
+  write.csv(as.data.frame(res),file.path(paste(path_to_deseq2_folder,cell_type_val,".csv")))
+
   print("wrote DE output")
 
   df_res <- as.data.frame(res) %>%
@@ -113,7 +135,8 @@ runDE_1_vs_all <- function(meta_col_to_use, cell_type_val) {
     dplyr::filter(log2FoldChange < -2 ) %>%
     dplyr::pull("gene")
 
-  print("DE func done")
+  print("_______________DEseq func done")
+
   return(list("DEG_down"= DEG_down, "DEG_up"= DEG_up))
 }
 
@@ -159,9 +182,11 @@ if (!dir.exists(file.path(opt$out_dir,"/UMAPs_pre_batch_correction/"))){
 if (!dir.exists(file.path(opt$out_dir,"/UMAPs_post_batch_correction/"))){
   dir.create(file.path(opt$out_dir,"/UMAPs_post_batch_correction/"),
               recursive = TRUE, showWarnings = TRUE)}
-if (!dir.exists(file.path(opt$out_dir,"/deseq2_outputs/"))){
-  dir.create(file.path(opt$out_dir,"/deseq2_outputs/"),
-              recursive = TRUE, showWarnings = TRUE)}
+
+
+#####################
+# Processing inputs
+#####################
 
 # Open files
 df_manifest <- read.csv(file = opt$manifest, sep = "\t", header=TRUE)
@@ -181,6 +206,8 @@ for (item in ls_kallisto_meta) {
 # Merge metadata from each data source by rows
 df_merged_metadata <- do.call("rbind", ls_meta)
 rownames(df_merged_metadata) <- c()
+
+print(ls_meta)
 
 # List of samples with LM22 labels
 ls_smpls_lm22 <- df_merged_metadata %>%
@@ -228,90 +255,192 @@ write.csv(metadata_summary,
            file.path(file.path(opt$out_dir,"metadata_summary.csv")),
            row.names = FALSE)
 
+##################
+# DESEQ2 LM22
+##################
+# Run deseq2 on each LM22 cell type vs all others
+# if("LM22" %in% colnames(df_merged_metadata_lm22)){
+#   ls_lm22_cell_types <-  unique(df_merged_metadata_lm22[["LM22"]])
+#   foreach(i=ls_lm22_cell_types, .packages=  c('magrittr', 'DESeq2')) %dopar% {
+#     runDE_1_vs_all(
+#         cell_type_val = i,
+#         meta_col_to_use="LM22"
+#         )
+#       }
+# }
+
+##################
+# DESEQ2 LM6
+##################
+# Run deseq2 on each LM6 cell type vs all others
+
+
+print("df_merged_metadata_lm22")
+# print(df_merged_metadata_lm22)
+
+if("LM6" %in% colnames(df_merged_metadata_lm22)){
+  ls_lm6_cell_types <-  unique(df_merged_metadata_lm22[["LM6"]])[1]
+  print(ls_lm6_cell_types)
+  print("-----")
+  foreach(i=ls_lm6_cell_types, .packages=  c('magrittr', 'DESeq2')) %dopar% {
+    runDE_1_vs_all(
+        cell_type_val = i,
+        meta_col_to_use="LM6"
+        )
+    }
+}
+
+# ##################
+# # Within Cell Type 
+# ##################
+# # Dont yet include types without samples or else nan filtering will break
+
+# # T_cell_types <- list(
+# #   "T cells CD8",
+# #   "T cells CD4 naive",
+# #   "T cells CD4 memory resting",
+# #   "T cells CD4 memory  activated",
+# #   "T cells follicular helper",
+# #   "T cells regulatory (Tregs)",
+# #   "T cells gamma delta")
+# T_cell_types <- list(
+#   "T cells CD8",
+#   "T cells CD4 naive",
+#   "T cells follicular helper",
+#   "T cells regulatory (Tregs)",
+#   "T cells gamma delta")
+
+# # mon_mac_cell_types <- list(
+# #   "Monocytes",
+# #   "Macrophages M0",
+# #   "Macrophages M1",
+# #   "Macrophages M2")
+# mon_mac_cell_types <- list(
+#     "Monocytes",
+#     "Macrophages M0",
+#     "Macrophages M1")
+
+# B_cell_types <- list(
+#   "B cells naive",
+#   "B cells memory")
+
+# dendritic_cell_types <- list(
+#   "Dendritic cells resting",
+#   "Dendritic cells activated")
+
+# # mast_cell_types <- list(
+# #   "Mast cells resting",
+# #   "Mast cells activated")
+
+# # NK_cell_types <- list(
+# #   "NK cells resting",
+# #   "NK cells activated")
+
+# # ls_within_cell_types <- list(
+# #   "T_cell_types" = T_cell_types,
+# #   "mon_mac_cell_types" = mon_mac_cell_types,
+# #   "Bcell" = B_cell_types,
+# #   "Dendritic" = dendritic_cell_types,
+# #   "Mast" = mast_cell_types,
+# #   "NK" = NK_cell_types)
+
+# ls_within_cell_types <- list(
+#   list("Tcell", T_cell_types),
+#   list("Mon_Mac", mon_mac_cell_types),
+#   list("Bcell", B_cell_types),
+#   list("Dendritic" ,dendritic_cell_types)
+# )
+
+# # Run Deseq2 for one cell type vs all other samples
+# runDE_1_vs_all_within_type <- function(ls_cell_types, label) {
+
+#   print(label)
+
+#   ls_cell_types <- unlist(ls_cell_types)
+#   print(ls_cell_types )
+  
+#   for (cell_type_val in ls_cell_types) %>%{
+
+#     # Make sample table to compare given cell type vs all other cell types
+#     sampleTable <- df_merged_metadata_lm22 %>%
+#       dplyr::select(Run, LM22, data_source, type) %>%
+#       dplyr::mutate(condition = ifelse(LM22 == cell_type_val, "main", "other"))
+
+#     print(head(sampleTable))
+
+#     runDEseq2(sampleTable,"LM22", cell_type_val)
+#     return(list("DEG_down"= DEG_down, "DEG_up"= DEG_up))
+#   }  
+# }
+
+# foreach(i=ls_within_cell_types, .packages=  c('magrittr', 'DESeq2')) %dopar% {
+#   runDE_1_vs_all_within_type(
+#       ls_gen_cell_types = i[2], 
+#       label= i[1]
+#       )
+#   }
+
 ####################################
 # UMAPs before batch correction
 ####################################
 
-# Drop genes with low variance.
-getVar <- apply(log2trans_dat[, -1], 1, var)
-param <- median(getVar)
-log2trans_dat_filt <- log2trans_dat[getVar > param & !is.na(getVar), ]
+# # Drop genes with low variance.
+# getVar <- apply(log2trans_dat[, -1], 1, var)
+# param <- median(getVar)
+# log2trans_dat_filt <- log2trans_dat[getVar > param & !is.na(getVar), ]
 
-# Transpose and format
-log2trans_dat_filt_t <- as.data.frame(t(log2trans_dat_filt)) %>%
-  dplyr::filter(!row.names(.) %in% c("gene"))
-rownames(log2trans_dat_filt_t) <- colnames(log2trans_dat_filt)
+# # Transpose and format
+# log2trans_dat_filt_t <- as.data.frame(t(log2trans_dat_filt)) %>%
+#   dplyr::filter(!row.names(.) %in% c("gene"))
+# rownames(log2trans_dat_filt_t) <- colnames(log2trans_dat_filt)
 
-# PCA.
-prcomp.out = as.data.frame(prcomp(log2trans_dat_filt_t, scale = F)$x)
-prcomp.out$Run = rownames(log2trans_dat_filt_t)
-prcomp.out.merge = merge(prcomp.out, y = log2trans_dat)
-
-# Making variations of UMAPs with different numbers of neighbors
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="LM22",
-  df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="sigil_general",
-  df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="data_source",
-  df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
-
-####################################
-# UMAPs after batch correction
-####################################
-
-# Batch correction
-log2trans_dat_batch_corr <- limma::removeBatchEffect(
-                                  log2trans_dat,
-                                  batch = df_merged_metadata_lm22$data_source,
-                                  batch2 = df_merged_metadata_lm22$type
-                                  )
-
-  write.csv(log2trans_dat_batch_corr,
-           file.path(file.path(opt$out_dir,"combined_kallisto_log2tpm_batch_corrected.csv")),
-           row.names = TRUE)
-
-# Drop genes with low variance.
-getVar_batch_corr <- apply(log2trans_dat_batch_corr[, -1], 1, var)
-param <- median(getVar_batch_corr)
-log2trans_dat_batch_corr_filt <- log2trans_dat_batch_corr[getVar_batch_corr > param & !is.na(getVar_batch_corr), ]
-
-# Transpose and format
-log2trans_dat_batch_corr_filt_t <- as.data.frame(t(log2trans_dat_batch_corr_filt)) %>%
-  dplyr::filter(!row.names(.) %in% c("gene"))
-rownames(log2trans_dat_batch_corr_filt_t) <- colnames(log2trans_dat_batch_corr_filt)
-
-# PCA.
-prcomp.out.batch.corr = as.data.frame(prcomp(log2trans_dat_batch_corr_filt_t, scale = F)$x)
-prcomp.out.batch.corr$Run = rownames(log2trans_dat_batch_corr_filt_t)
+# # PCA.
+# prcomp.out = as.data.frame(prcomp(log2trans_dat_filt_t, scale = F)$x)
+# prcomp.out$Run = rownames(log2trans_dat_filt_t)
 # prcomp.out.merge = merge(prcomp.out, y = log2trans_dat)
 
-# Making variations of UMAPs with different numbers of neighbors
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="LM22",
-  df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="sigil_general",
-  df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
-lapply(c(5,10,15,20,25,30), make_umap, meta_col="data_source",
-  df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
+# # Making variations of UMAPs with different numbers of neighbors
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="LM22",
+#   df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="sigil_general",
+#   df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="data_source",
+#   df_PCA = prcomp.out, out_path = "UMAPs_pre_batch_correction/kallisto_PCA_UMAP")
 
-##################
-# DESEQ2 LM22
-##################
+# ####################################
+# # UMAPs after batch correction
+# ####################################
 
-# Run deseq2 on each LM22 cell type vs all others
-if("LM22" %in% colnames(df_merged_metadata_lm22)){
-  print("Running...")
+# # Batch correction
+# log2trans_dat_batch_corr <- limma::removeBatchEffect(
+#                                   log2trans_dat,
+#                                   batch = df_merged_metadata_lm22$data_source,
+#                                   batch2 = df_merged_metadata_lm22$type
+#                                   )
 
-  # Format DF to label samples(columns) with general and more specific labels
-  df_sample_annotations <- df_merged_metadata_lm22 %>%
-    dplyr::select(Run,sigil_general,data_source,LM22,type) %>%
-    tibble::column_to_rownames("Run")
+#   write.csv(log2trans_dat_batch_corr,
+#            file.path(file.path(opt$out_dir,"combined_kallisto_log2tpm_batch_corrected.csv")),
+#            row.names = TRUE)
 
-  print(head(df_sample_annotations))
+# # Drop genes with low variance.
+# getVar_batch_corr <- apply(log2trans_dat_batch_corr[, -1], 1, var)
+# param <- median(getVar_batch_corr)
+# log2trans_dat_batch_corr_filt <- log2trans_dat_batch_corr[getVar_batch_corr > param & !is.na(getVar_batch_corr), ]
 
-  # Run DE 1 cell type vs all others
-  DE_results <- sapply(
-    unique(df_merged_metadata_lm22[["LM22"]]),
-    runDE_1_vs_all,
-    meta_col_to_use="LM22")
+# # Transpose and format
+# log2trans_dat_batch_corr_filt_t <- as.data.frame(t(log2trans_dat_batch_corr_filt)) %>%
+#   dplyr::filter(!row.names(.) %in% c("gene"))
+# rownames(log2trans_dat_batch_corr_filt_t) <- colnames(log2trans_dat_batch_corr_filt)
 
-}
+# # PCA.
+# prcomp.out.batch.corr = as.data.frame(prcomp(log2trans_dat_batch_corr_filt_t, scale = F)$x)
+# prcomp.out.batch.corr$Run = rownames(log2trans_dat_batch_corr_filt_t)
+# # prcomp.out.merge = merge(prcomp.out, y = log2trans_dat)
+
+# # Making variations of UMAPs with different numbers of neighbors
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="LM22",
+#   df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="sigil_general",
+#   df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
+# lapply(c(5,10,15,20,25,30), make_umap, meta_col="data_source",
+#   df_PCA = prcomp.out.batch.corr, out_path = "UMAPs_post_batch_correction/kallisto_PCA_UMAP")
