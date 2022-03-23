5ea2543d75cc40cc75c2cef059274e537e535425
althornt <alxs412@gmail.com>
2022-03-23 02:57:30 +0000

    add gene explore script

---
+++ bin/exploreGeneRefMatrix.R
@@ -0,0 +1,220 @@
+#!/usr/bin/env Rscript
+library(optparse)
+library(magrittr)
+library(pheatmap)
+library(ggplot2)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(uwot)
+library(RColorBrewer)
+library(foreach)
+library(doParallel)
+cl <- makeCluster(detectCores() - 1, outfile = "")
+registerDoParallel(cl)
+
+save_pheatmap_pdf <- function(x, filename, width=7, height=7) {
+  #' Function to save pheatmaps to a pdf file
+  stopifnot(!missing(x))
+  stopifnot(!missing(filename))
+  pdf(filename, width=width, height=height)
+  grid::grid.newpage()
+  grid::grid.draw(x$gtable)
+  dev.off()
+}
+
+make_pheatmap <- function(ls_events, label, df_meta, df_PS){
+  #' Make heatmap using the pheatmap package using the given events
+  #' and data
+  #'
+  #' @param ls_events - list of events of interest to use in heatmap
+  #' @param label - label to use in output file path
+  #' @param df_meta - df of metadata with Run, val, and data_source, LM6, LM22 columns
+  #' @param df_PS - df of MESA all PS file
+
+  # Filter MESA all PS file to events of interest
+  df_all_PS_sig_events <- df_PS %>%
+    tibble::rownames_to_column('event') %>%
+    dplyr::filter(event %in% ls_events) %>%
+    dplyr::select(noquote(order(colnames(.)))) %>%
+    tibble::column_to_rownames('event')
+
+  # print(df_all_PS_sig_events)
+
+  for (val in list("LM22", "LM6")){
+      if (nrow(df_all_PS_sig_events) < 50){
+        rowname_on_off = "T"
+      } else { rowname_on_off = "F"}
+
+      # DF to label samples(columns) with labels
+      df_sample_annotations <- df_meta %>%
+        dplyr::filter(paste0(val) != "") %>%
+        dplyr::select(Run, val, data_source) %>%
+        dplyr::arrange(Run) %>%
+        tibble::column_to_rownames("Run")
+
+      stopifnot(rownames(df_sample_annotations) == colnames(df_all_PS_sig_events))
+
+      heatmap_res <- pheatmap(
+        main = paste0(" "),
+        df_all_PS_sig_events,
+        # scale = "row",
+        show_rownames=get(rowname_on_off),
+        show_colnames=F,
+        na_col = "grey",
+        annotation_col = df_sample_annotations)
+
+      save_pheatmap_pdf(
+        heatmap_res,
+        paste0(opt$out_dir,"/",label,"_",val,"_rowname",rowname_on_off,".pdf"))
+      }
+}
+
+calcGroupMed <- function(df_PS, LM_type){
+  str_LM <- as.character(LM_type)
+
+  #transpose df, summarize to median of each group
+  df_t_med <- df_exp_ref_events %>%
+    rownames_to_column("rowname") %>%   #transpose
+    gather(var, value, -rowname) %>% 
+    spread(rowname, value) %>%
+    filter(get(LM_type) != "") %>%     #remove samples without a LM grouping
+    select(c(paste0(str_LM),"var",df_geneRefMatrix$X )) %>% #keep LM col and junctions
+    mutate_at(df_geneRefMatrix$X, as.numeric) %>% # convert to numeric
+    group_by_at(LM_type) %>%
+    summarise_at(vars(df_geneRefMatrix$X), funs(median(., na.rm=TRUE))) %>%
+    column_to_rownames(paste(str_LM)) %>%
+    as.data.frame(.) %>%
+    select_if(~ !any(is.na(.))) #remove NA
+
+  df_med <-  df_t_med  %>%
+    rownames_to_column("rowname") %>% 
+    gather(var, value, -rowname)  %>% 
+    spread(rowname, value) %>% 
+    as.data.frame(.) %>%
+    column_to_rownames("var")
+    
+  #Remove junctions with 0 variance which break scaling
+  df_med_var<- df_med[apply(df_med, 1, var) != 0, ]
+
+  heatmap_res <- pheatmap(
+          main = paste0(" "),
+          df_med_var,
+          scale = "row",
+          show_rownames=F,
+          show_colnames=T,
+          na_col = "grey"
+          )
+
+  save_pheatmap_pdf(
+          heatmap_res,
+          paste0(opt$out_dir,"/",paste(LM_type),"_med_heatmap.pdf")
+          )
+
+  return(df_med)
+}
+
+# Arguments
+option_list <- list(
+  optparse::make_option(
+    c("-s", "--geneRefMatrix"),
+    type = "character",
+    default = NULL,
+    help = "gene reference matrix"),
+
+  optparse::make_option(
+    c("-o", "--out_dir"),
+    type = "character",
+    default = NULL,
+    help = "full path to put outputs"),
+
+  optparse::make_option(
+    c("-m", "--metadata"),
+    type = "character",
+    default = NULL,
+    help = "full path to put outputs"),
+      
+  optparse::make_option(
+    c("-i", "--input"),
+    type = "character",
+    default = NULL,
+    help = " `combined and batch corrected input file ")
+    )
+
+# Read the arguments passed
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
+
+# Make output directories
+if (!dir.exists(paste0(opt$out_dir))){
+  dir.create(paste0(opt$out_dir),
+   recursive = TRUE, showWarnings = TRUE)
+}
+
+# Open files
+df_geneRefMatrix <- readr::read_tsv(file = opt$geneRefMatrix)
+print(head(df_geneRefMatrix))
+print(dim(df_geneRefMatrix))
+
+# df_exp <- read.table(file = opt$i, sep=",", header = TRUE, row.names=1) 
+# df_exp <- df_exp %>% mutate_if(is.character,as.numeric)
+
+# print(head(df_exp))
+
+# metadata <- read.csv(file = opt$metadata)
+# df_sample_annotations <- metadata %>%
+#   dplyr::select(Run,LM22,LM6, sigil_general, data_source) %>%
+#   tibble::column_to_rownames("Run") %>%
+#   t()
+
+# # Add metadata to splice ref matrix
+# df_exp_meta <- rbind(df_exp, df_sample_annotations)
+
+# df_exp_ref_events <- df_exp_meta %>%
+#   rownames_to_column("col") %>%
+#   filter((col %in% df_geneRefMatrix$X)|(col %in% rownames(df_sample_annotations))) %>%
+#   column_to_rownames("col")
+
+# print(head(df_exp_ref_events))
+# print(dim(df_exp_ref_events))
+
+
+# ######################################################
+# # RefMat with medians + z-score
+# ######################################################
+# # Calculate group medians and make heatmap then z-score
+# df_LM6_med <- calcGroupMed(df_exp_ref_events, "LM6")
+# df_LM6_med_z <- t(scale(t(df_LM6_med)))
+
+# df_LM22_med <- calcGroupMed(df_exp_ref_events, "LM22")
+# df_LM22_med_z <- t(scale(t(df_LM22_med)))
+
+# print(dim(df_LM6_med))
+# print(dim(df_LM6_med_z))
+
+# print(dim(df_LM22_med))
+# print(dim(df_LM22_med_z))
+
+######################################################
+# Count gene occurances in matrix
+######################################################
+
+print(head(df_geneRefMatrix))
+
+df_geneRefMatrix_count <- df_geneRefMatrix %>%
+    group_by(X) %>%
+    summarize(context = list(cell_type)) %>%
+  mutate(cell_types = map_chr(context, toString)) %>%
+  select(X,cell_types) 
+
+write.csv(df_geneRefMatrix_count, 
+            paste0(opt$out_dir,"/gene_occurances.csv"))
+
+# Plot distribution of counts per gene 
+df_hist_gene <- df_geneRefMatrix %>% 
+  count(X, sort = TRUE) 
+ggplot(df_hist_gene, aes(x=n)) + 
+  geom_histogram() + 
+  scale_x_continuous(breaks = round(seq(min(df_hist_gene$n), max(df_hist_gene$n), by = 1),1)) +
+  ggtitle("Counts per gene in the gene reference matrix") 
+ggsave( paste0(opt$out_dir,"/hist_count_per_gene.png"))
