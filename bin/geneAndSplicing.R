f74fe7fa5f1a9dfd46311e0137d54cf283bb075a
althornt <alxs412@gmail.com>
2022-03-25 04:19:40 +0000

    add lm22 scatter

--- bin/geneAndSplicing.R
+++ bin/geneAndSplicing.R
@@ -15,7 +15,63 @@ library(UpSetR)
 cl <- makeCluster(detectCores() - 1, outfile = "")
 registerDoParallel(cl)
 
-print("hello")
+
+########################
+# Functions
+#########################
+format_merge <- function(df_gene,  df_splice){
+  # Making long dfs
+  df_gene_z_long <- df_gene %>%
+      tidyr::gather(., key="cell_type", value = gene_z, -c(gene)) %>%
+      as.data.frame()
+
+  df_splice_z_long <- df_splice %>%
+      rename(cell_type_sig_splice = cell_type) %>%
+      tidyr::gather(., key="cell_type", value = splice_z, -c(gene, event, cell_type_sig_splice)) %>%
+      as.data.frame()
+
+  # Merging the long dfs 
+  df_merged_z_long <-  df_splice_z_long %>%
+          inner_join(df_gene_z_long, by = c("gene","cell_type"), suffix=c("_splice","_gene")) %>% 
+          tidyr::drop_na()
+
+  return(df_merged_z_long)
+
+}
+scatter_per_gene <- function(df_long, ouput_prefix){
+
+  # Make scatter plot for each matching gene
+  ls_genes <- unique(df_long$gene)
+  for (g in ls_genes){
+    df_g <- df_long %>%
+      filter(gene == g)
+    p <- ggplot(aes(x=gene_z, y=splice_z, color = cell_type), data=df_g)+ 
+      geom_point() +
+      labs(title= g) + geom_hline(yintercept = 0) +  geom_vline(xintercept = 0) +
+      geom_text(
+                label= df_g$cell_type_sig_splice,
+                nudge_x = 0.05, nudge_y = 0.05,
+                check_overlap =F, col = "black", size = 1.5
+              ) +
+      theme_minimal()
+
+    ggsave(plot = p, filename = paste0(ouput_prefix, g, ".png"))
+  }
+
+}
+
+
+scatter_all <- function(df_long, output_path){
+  # Scatter plot with all matching events/genes
+  p <- ggplot(aes(x=gene_z, y=splice_z, color = cell_type), data=df_long)+ 
+    geom_point() +
+    geom_text(
+              label= df_long$gene,
+              nudge_x = 0.05, nudge_y = 0.05,
+              check_overlap =F, col = "black", size = 1.5
+            )
+  ggsave(plot = p, filename = output_path)
+}
 
 # Arguments
 option_list <- list(
@@ -44,6 +100,15 @@ opt <- optparse::parse_args(opt_parser)
 print(opt$spliceDir)
 print(opt$geneDir)
 
+
+# Make output directories
+if (!dir.exists(file.path(opt$out_dir,"/LM6_scatterplots/"))){
+  dir.create(file.path(opt$out_dir,"/LM6_scatterplots/"),
+              recursive = TRUE, showWarnings = TRUE)}
+if (!dir.exists(file.path(opt$out_dir,"/LM22_scatterplots/"))){
+  dir.create(file.path(opt$out_dir,"/LM22_scatterplots/"),
+              recursive = TRUE, showWarnings = TRUE)}
+
 # Read in files 
 
 # Reference matrix 
@@ -52,6 +117,11 @@ df_event2gene <- read.csv(file= paste0(opt$spliceDir,"/ref_matrix/lm22_lm6_withi
 print("splice ref mat")
 print(head(df_event2gene))
 print(dim(df_event2gene))
+
+
+#######################
+# LM6
+########################
 
 df_lm6_splice_z <- read.csv(file= paste0(opt$spliceDir,"explore_ref_matrix/LM6_med_zscore.csv"), header=TRUE) %>%
       rename(event = X)
@@ -77,74 +147,50 @@ print(dim(df_lm6_gene_z))
 # merge z_score dfs 
 df_merged_z <- df_lm6_gene_z %>%
         inner_join(df_lm6_splice_z_gene, by = "gene", suffix=c("_splice","_gene"))
-# print(head(df_lm6_gene_z))
-# print(head(df_lm6_splice_z_gene))
 print("merged")
 print(head(df_merged_z))
 print(dim(df_merged_z))
 
-# df_merged_z_long <- df_merged_z %>%
-#     tidyr::gather(., key="idk", value = z, -c(gene, cell_type)) %>%
-#     as.data.frame()
-# print("df_lm6_gene_z_long")
-# print(head(df_merged_z_long))
+# Scatterplots 
+df_lm6_merged_z_long <- format_merge(df_lm6_gene_z,df_lm6_splice_z_gene )
+scatter_all(df_lm6_merged_z_long, paste0(opt$out_dir, "/lm6_scatter_color_by_splice_cell_type_all.png"))
+scatter_per_gene(df_lm6_merged_z_long, paste0(opt$out_dir, "/LM6_scatterplots/"))
 
 
-#####################################
-# Scatter plots gene vs splicing 
-#####################################
-# Formatting long dfs
-df_lm6_gene_z_long <- df_lm6_gene_z %>%
-    tidyr::gather(., key="cell_type", value = gene_z, -c(gene)) %>%
-    as.data.frame()
-  
-print("df_lm6_gene_z_long")
-print(head(df_lm6_gene_z_long))
+# #######################
+# # LM22
+# ########################
 
-df_lm6_splice_z_long <- df_lm6_splice_z_gene %>%
-    rename(cell_type_sig_splice = cell_type) %>%
-    tidyr::gather(., key="cell_type", value = splice_z, -c(gene, event, cell_type_sig_splice)) %>%
-    as.data.frame()
+df_lm22_splice_z <- read.csv(file= paste0(opt$spliceDir,"explore_ref_matrix/LM22_med_zscore.csv"), header=TRUE) %>%
+      rename(event = X)
+print("lm22 splice z")
+print(head(df_lm22_splice_z))
+print(dim(df_lm22_splice_z))
 
-print("--------------")
-print("df_lm6_splice_z_long")
-print(head(df_lm6_splice_z_long))
+# Add gene name to splice events
+df_lm22_splice_z_gene <- merge(x=df_event2gene,y=df_lm22_splice_z,by="event",all=TRUE) %>%
+    select( -column_label2,-group, -column_label) %>%
+    rename(gene = overlapping)
 
-# Merging the long dfs 
-df_merged_z_long <-  df_lm6_splice_z_long %>%
-        inner_join(df_lm6_gene_z_long, by = c("gene","cell_type"), suffix=c("_splice","_gene")) %>% 
-         tidyr::drop_na()
-print("merged z long")
-print(head(df_merged_z_long))
-print(dim(df_merged_z_long))
+print("lm22 splice z gene name")
+print(head(df_lm22_splice_z_gene))
+print(dim(df_lm22_splice_z_gene))
 
+df_lm22_gene_z <- read.csv(file= paste0(opt$geneDir,"explore_ref_matrix/LM22_med_zscore.csv"), header=TRUE) %>%
+    rename(gene = X) 
+print("lm22 gene")
+print(head(df_lm22_gene_z))
+print(dim(df_lm22_gene_z))
 
-# Scatter plot with all matching events/genes
-p <- ggplot(aes(x=gene_z, y=splice_z, color = cell_type), data=df_merged_z_long)+ 
-  geom_point() +
-  geom_text(
-            label= df_merged_z_long$gene,
-            nudge_x = 0.05, nudge_y = 0.05,
-            check_overlap =F, col = "black", size = 1.5
-          )
-ggsave(plot = p, filename = paste0(opt$out_dir, "/lm6_scatter_color_by_splice_cell_type_all.png"))
+# merge z_score dfs 
+df_lm22_merged_z <- df_lm22_gene_z %>%
+        inner_join(df_lm22_splice_z_gene, by = "gene", suffix=c("_splice","_gene"))
+print("merged")
+print(head(df_lm22_merged_z))
+print(dim(df_lm22_merged_z))
 
-
-# Make scatter plot for each matching gene
-ls_genes <- unique(df_merged_z_long$gene)
-for (g in ls_genes){
-  df_g <- df_merged_z_long %>%
-    filter(gene == g)
-  p <- ggplot(aes(x=gene_z, y=splice_z, color = cell_type), data=df_g)+ 
-    geom_point() +
-    labs(title= g) + geom_hline(yintercept = 0) +  geom_vline(xintercept = 0) +
-    geom_text(
-              label= df_g$cell_type_sig_splice,
-              nudge_x = 0.05, nudge_y = 0.05,
-              check_overlap =F, col = "black", size = 1.5
-            ) +
-    theme_minimal()
-  ggsave(plot = p, filename = paste0(opt$out_dir, "/lm6_scatter_color_by_splice_cell_type_", g, ".png"))
-}
-
+# Scatterplots
+df_lm22_merged_z_long <- format_merge(df_lm22_gene_z,df_lm22_splice_z_gene )
+scatter_all(df_lm22_merged_z_long, paste0(opt$out_dir, "/lm22_scatter_color_by_splice_cell_type_all.png"))
+scatter_per_gene(df_lm22_merged_z_long, paste0(opt$out_dir, "/LM22_scatterplots/"))
 
