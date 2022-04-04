5c8b91b75c46c58c0ab408c72bd48e1bfda8afd8
althornt <alxs412@gmail.com>
2022-04-04 01:05:56 +0000

    add splice ref events vs gene scatterplot

--- bin/geneAndSplicing.R
+++ bin/geneAndSplicing.R
@@ -197,99 +197,304 @@ df_splice_ref <- read.csv(file= paste0(
 print("splice ref mat")
 print(head(df_splice_ref))
 print(dim(df_splice_ref))
-
 print(unique(df_splice_ref$group))
 
-# Map events to their signifcant cell type
-df_event2cell <- df_splice_ref %>%
-    group_by(event) %>%
-    summarize(context = list(cell_type)) %>%
-    mutate(cell_types = map_chr(context, toString)) %>%
-    select(event, cell_types) %>%
-    as.data.frame()
-print(head(df_event2cell))
-print(dim(df_event2cell))
 
-df_event2cell <- merge(x = df_event2cell, y = df_splice_ref, by="event") %>%
-  distinct(event, .keep_all = TRUE)
+#############
+# MAIN PLOT: 
+##############################
+# LM22 splice ref vs all matched genes
+##############################
+# Adding Z-scores into the splice ref df 
 
-print(head(df_event2cell))
-print(dim(df_event2cell))
+# Merge cols to add LM tag (within type uses LM22 groups )
+df_splice_ref <- df_splice_ref %>%
+  mutate(cell_type_group = ifelse(group=="LM6",
+                                paste(cell_type, "LM6", sep = "_"),
+                                paste(cell_type, "LM22", sep = "_")))
+print(head(df_splice_ref))
 
-print("=====================================================================")
+##################################
+# Formating Gene Exp Z-score df
+#################################
 
-# Genes in gene reference matrix 
-df_gene_ref <- read.csv(file= paste0(
+# Read in all gene exp 
+df_exp <- read.table(file= paste0(
                               opt$geneDir,
-                              "/ref_matrix/lm22_lm6_withinType_combinedRefMat.tsv"),
-                         sep = "\t",header=TRUE) %>%
-              rename(gene = X)
-print("gene ref mat")
-print(head(df_gene_ref))
+                              "combined_kallisto_log2tpm_batch_corrected.csv"),
+                    sep=",", header = TRUE, row.names=1)
+df_exp <- df_exp %>% mutate_if(is.character,as.numeric)
+print(head(df_exp))
+print(dim(df_exp))
 
-print(unique(df_gene_ref$group))
-print(dim(df_gene_ref))
+# Gene exp : get medians and z-score across group
+df_exp_LM22_med <- calcGroupMed(df_exp, unlist(unique(df_splice_ref$overlapping)), "LM22")
+df_exp_LM22_med_z <- t(scale(t(df_exp_LM22_med)))
+df_exp_LM6_med <- calcGroupMed(df_exp, unlist(unique(df_splice_ref$overlapping)), "LM6")
+df_exp_LM6_med_z <- t(scale(t(df_exp_LM6_med)))
 
+# Add LM6 / LM22 tags to column names because same cell name can be in both
+colnames(df_exp_LM22_med_z) <- paste(colnames(df_exp_LM22_med_z),"LM22",sep=" ")
+colnames(df_exp_LM6_med_z) <- paste(colnames(df_exp_LM6_med_z),"LM6",sep=" ")
 
-# Map genes to to their signifcant cell type
-df_DEG2cell <- df_gene_ref %>%
-    group_by(gene) %>%
-    summarize(context = list(cell_type)) %>%
-    mutate(cell_types = map_chr(context, toString)) %>%
-    select(gene, cell_types) %>%
-    as.data.frame()
+# Gene exp: Combine LM6 and LM22 dfs
+df_exp_LM6_LM22_med_z <- cbind(df_exp_LM6_med_z, df_exp_LM22_med_z)
+print("Combined......")
+print(head(df_exp_LM6_LM22_med_z))
+print(dim(df_exp_LM6_LM22_med_z))
 
-print(head(df_DEG2cell))
-print(dim(df_DEG2cell))
+# Replace spaces in names with _ to match other df
+str_conv_space <- function(in_str){
+  paste(unlist(strsplit(as.character(in_str), split=" ")),
+                                    collapse="_")
+}
 
-#######################
-# LM6 group z_scores
-########################
+# Replace spaces with _ to match other df
+new_exp_names <- unlist(lapply(colnames(df_exp_LM6_LM22_med_z),FUN= str_conv_space))
+colnames(df_exp_LM6_LM22_med_z) <- new_exp_names
+print(head(df_exp_LM6_LM22_med_z))
 
-# Open LM6 splice z scores 
-df_lm6_splice_z <- read.csv(file= paste0(opt$spliceDir,
-                                      "explore_ref_matrix/LM6_med_zscore.csv"),
-                            header=TRUE) %>%
-                    rename(event = X)
-
-# Add sig cell type and gene name
-df_lm6_splice_z <-  merge(x=df_lm6_splice_z,y=df_event2cell,by="event",all=TRUE) %>%
-    select( -column_label2,-group, -column_label, -cell_type) %>%
-    rename(gene = overlapping)
-
-print("----------------------------------------")
-print(head(df_lm6_splice_z))
-print(dim(df_lm6_splice_z))
-
-# Open LM6 gene z scores 
-df_lm6_gene_z <- read.csv(file= paste0(opt$geneDir,"explore_ref_matrix/LM6_med_zscore.csv"), header=TRUE) %>%
-    rename(gene = X)
-# Add  cell_type to z scores
-df_lm6_gene_z <- merge(x=df_DEG2cell,y=df_lm6_gene_z,by="gene",all=TRUE) 
-print("df_lm6_gene_z")
-print(head(df_lm6_gene_z))
-print(dim(df_lm6_gene_z))
-
-
-# # Scatterplots 
-# df_lm6_merged_z_long <- format_merge(df_lm6_gene_z,df_lm6_splice_z )
-# print(head(df_lm6_merged_z_long))
-# scatter_all(df_lm6_merged_z_long, paste0(opt$out_dir, "/lm6_scatter_all.png"))
-# scatter_per_gene(df_lm6_merged_z_long, paste0(opt$out_dir, "/LM6_scatterplots/"))
-
-# #######################
-# # LM22
-# ########################
-# Open LM22 splice z scores 
+# Confirm new columns str format matches ref mat rows
+# Add stop if not equal 
+print(dim(df_splice_ref))
+print(dim(df_splice_ref %>% filter(cell_type_group %in% colnames(df_exp_LM6_LM22_med_z))))
+print("############################")
+##################################
+# Formating Splice Z-score df
+#################################
 df_lm22_splice_z <- read.csv(file= paste0(opt$spliceDir,
                                       "explore_ref_matrix/LM22_med_zscore.csv"),
-                            header=TRUE) %>%
-                    rename(event = X)
+                            header=TRUE, check.names=FALSE, row.names = 1)
 
-# Add sig cell type and gene name
-df_lm22_splice_z <-  merge(x=df_lm22_splice_z,y=df_event2cell,by="event",all=TRUE) %>%
-    select( -column_label2,-group, -column_label, -cell_type) %>%
-    rename(gene = overlapping)
+
+print(head(df_lm22_splice_z))
+df_lm6_splice_z <- read.csv(file= paste0(opt$spliceDir,
+                                      "explore_ref_matrix/LM6_med_zscore.csv"),
+                            header=TRUE, check.names=FALSE, row.names = 1) 
+print(head(df_lm6_splice_z))
+
+# Add LM6 / LM22 tags to column names because same cell name can be in both
+colnames(df_lm22_splice_z) <- paste(colnames(df_lm22_splice_z),"LM22",sep=" ")
+colnames(df_lm6_splice_z) <- paste(colnames(df_lm6_splice_z),"LM6",sep=" ")
+
+print(head(df_lm6_splice_z))
+print(head(df_lm22_splice_z))
+
+
+# Merge by row.names (cant cbind due to different number of events likely due to NANs)
+df_splice_LM6_LM22_med_z <- merge(df_lm6_splice_z,df_lm22_splice_z,by=0) %>%
+  column_to_rownames("Row.names")
+
+# Splice: Combine LM6 and LM22
+# df_splice_LM6_LM22_med_z <- cbind(df_lm6_splice_z, df_lm22_splice_z)
+print(dim(df_lm6_splice_z))
+print(dim(df_lm22_splice_z))
+print(dim(df_splice_LM6_LM22_med_z))
+print(head(df_splice_LM6_LM22_med_z))
+
+# Replace spaces with _ to match other df
+new_splice_names <- unlist(lapply(colnames(df_splice_LM6_LM22_med_z),FUN= str_conv_space))
+colnames(df_splice_LM6_LM22_med_z) <- new_splice_names
+print(head(df_splice_LM6_LM22_med_z))
+
+# Check new columns str format matches ref mat rows
+# Add stop if not equal 
+print(dim(df_splice_ref))
+print(dim(df_splice_ref %>% filter(cell_type_group %in% colnames(df_splice_LM6_LM22_med_z))))
+
+#################################################
+# Adding z-scores into df of splice ref events 
+#################################################
+print("###############################################################")
+# All genes that were in splice ref matrix
+df_splice_ref_genes <- unique(df_splice_ref$overlapping)
+print(length(df_splice_ref_genes))
+
+df_splice_ref['gene_z'] <- NA
+df_splice_ref['splice_z'] <- NA
+print(head(df_splice_ref))
+
+print(head(df_exp_LM6_LM22_med_z))
+
+# Adding gene z_scores to splice ref df
+for (gene in df_splice_ref_genes){
+  # print("start.............")
+  # print(gene)
+  #  For each gene get the cell_types the splice event was signifcant in
+  df_  <- df_splice_ref %>% filter(overlapping == gene)
+  # print(as.vector(unlist(unique(df_$cell_type))))
+  # print(df_)
+
+  # Check if gene in gene exp data (splicing can have lists for overlaps "AC129492.1,AC129492.4")
+  if (gene %in% rownames(df_exp_LM6_LM22_med_z)){
+    
+    # Iterate over cell types
+    for (gene_cell in as.vector(unlist(unique(df_$cell_type_group)))){
+      # print(gene_cell)
+
+      # Look up the gene exp val for gene and cell type
+      z <- df_exp_LM6_LM22_med_z[gene, gene_cell] 
+      # print(z)
+
+      # Add gene z to each corresponding splice ref rows
+      df_splice_ref <- df_splice_ref %>%
+        mutate(gene_z = ifelse(((overlapping==gene) & (cell_type_group==gene_cell)), z, gene_z))
+    }
+  } else
+  {print("no gene match")}
+}
+
+
+print(head(df_splice_ref))
+# print(df_splice_ref)
+
+# Adding splice z_scores to splice ref df
+for (this_event in df_splice_ref$event){
+  print("start.................")
+  print(this_event)
+  df_  <- df_splice_ref %>% filter(event == this_event)
+  print(df_)
+
+  # Iterate over cell types
+  for (splice_cell in as.vector(unlist(unique(df_$cell_type_group)))){
+    print(splice_cell)
+    print(df_ %>% filter(cell_type_group ==splice_cell ))
+
+    z <- df_splice_LM6_LM22_med_z[this_event, splice_cell] 
+    print(z)
+
+    print(df_splice_LM6_LM22_med_z %>% select(splice_cell) %>% filter(row.names(.)==this_event))
+
+    df_splice_ref <- df_splice_ref %>%
+       mutate(splice_z = ifelse(((event==this_event) & (cell_type_group==splice_cell)), z, splice_z))
+}
+}
+
+
+print(head(df_splice_ref))
+
+# for (label_type in c(cell_type, overlapping, ))
+
+p <- ggplot(aes(x=splice_z, y=gene_z, color = cell_type), data=df_splice_ref)+ 
+  geom_point(size=.5) +
+  labs(title= "") + 
+  geom_hline(yintercept = 0, size = .25, linetype='dotted', color = "grey") +  
+  geom_vline(xintercept = 0, size = .25, linetype='dotted', color = "grey") +
+  geom_text(
+            label= df_splice_ref$overlapping,
+            nudge_x = 0.05, nudge_y = 0.05,
+            check_overlap =F, col = "black", size = 1
+          ) +
+  theme_minimal() +
+  theme(legend.position="bottom", 
+        legend.title = element_text(size = 5), 
+        legend.text = element_text(size = 5)) 
+        # +
+  # guides(color = guide_legend(nrow = 3))
+
+ggsave(plot = p, filename = paste0(opt$out_dir, "/splice_ref_vs_gene.png"))
+
+	# - for each reference matrix splice junction
+	# 	- for that cell type pull the gene expression from the big table
+	# 	- z-score all
+	# 	- scatter plot one labeling gene
+	# 	- another labeling event
+	# 	- another labeling cell-type sig
+	# 	- only plot the z-score for the cell_type specific one 
+
+
+
+
+# # Map events to their signifcant cell type
+# df_event2cell <- df_splice_ref %>%
+#     group_by(event) %>%
+#     summarize(context = list(cell_type)) %>%
+#     mutate(cell_types = map_chr(context, toString)) %>%
+#     select(event, cell_types) %>%
+#     as.data.frame()
+# print(head(df_event2cell))
+# print(dim(df_event2cell))
+
+# df_event2cell <- merge(x = df_event2cell, y = df_splice_ref, by="event") %>%
+#   distinct(event, .keep_all = TRUE)
+
+# print(head(df_event2cell))
+# print(dim(df_event2cell))
+
+# print("=====================================================================")
+
+# # Genes in gene reference matrix 
+# df_gene_ref <- read.csv(file= paste0(
+#                               opt$geneDir,
+#                               "/ref_matrix/lm22_lm6_withinType_combinedRefMat.tsv"),
+#                          sep = "\t",header=TRUE) %>%
+#               rename(gene = X)
+# print("gene ref mat")
+# print(head(df_gene_ref))
+
+# print(unique(df_gene_ref$group))
+# print(dim(df_gene_ref))
+
+
+# # Map genes to to their signifcant cell type
+# df_DEG2cell <- df_gene_ref %>%
+#     group_by(gene) %>%
+#     summarize(context = list(cell_type)) %>%
+#     mutate(cell_types = map_chr(context, toString)) %>%
+#     select(gene, cell_types) %>%
+#     as.data.frame()
+
+# print(head(df_DEG2cell))
+# print(dim(df_DEG2cell))
+
+# #######################
+# # LM6 group z_scores
+# ########################
+
+# # Open LM6 splice z scores 
+# df_lm6_splice_z <- read.csv(file= paste0(opt$spliceDir,
+#                                       "explore_ref_matrix/LM6_med_zscore.csv"),
+#                             header=TRUE) %>%
+#                     rename(event = X)
+
+# # Add sig cell type and gene name
+# df_lm6_splice_z <-  merge(x=df_lm6_splice_z,y=df_event2cell,by="event",all=TRUE) %>%
+#     select( -column_label2,-group, -column_label, -cell_type) %>%
+#     rename(gene = overlapping)
+
+# print("----------------------------------------")
+# print(head(df_lm6_splice_z))
+# print(dim(df_lm6_splice_z))
+
+# # Open LM6 gene z scores 
+# df_lm6_gene_z <- read.csv(file= paste0(opt$geneDir,"explore_ref_matrix/LM6_med_zscore.csv"), header=TRUE) %>%
+#     rename(gene = X)
+# # Add  cell_type to z scores
+# df_lm6_gene_z <- merge(x=df_DEG2cell,y=df_lm6_gene_z,by="gene",all=TRUE) 
+# print("df_lm6_gene_z")
+# print(head(df_lm6_gene_z))
+# print(dim(df_lm6_gene_z))
+
+
+# # # Scatterplots 
+# # df_lm6_merged_z_long <- format_merge(df_lm6_gene_z,df_lm6_splice_z )
+# # print(head(df_lm6_merged_z_long))
+# # scatter_all(df_lm6_merged_z_long, paste0(opt$out_dir, "/lm6_scatter_all.png"))
+# # scatter_per_gene(df_lm6_merged_z_long, paste0(opt$out_dir, "/LM6_scatterplots/"))
+
+# # #######################
+# # # LM22
+# # ########################
+# # Open LM22 splice z scores 
+# df_lm22_splice_z <- read.csv(file= paste0(opt$spliceDir,
+#                                       "explore_ref_matrix/LM22_med_zscore.csv"),
+#                             header=TRUE) %>%
+#                     rename(event = X)
+
+# # Add sig cell type and gene name
+# df_lm22_splice_z <-  merge(x=df_lm22_splice_z,y=df_event2cell,by="event",all=TRUE) %>%
+#     select( -column_label2,-group, -column_label, -cell_type) %>%
+#     rename(gene = overlapping)
 
 # print("----------------------------------------")
 # print(head(df_lm22_splice_z))
@@ -336,118 +541,4 @@ df_lm22_splice_z <-  merge(x=df_lm22_splice_z,y=df_event2cell,by="event",all=TRU
 
 # print("Number of spliced genes with matched gene in gene reference matrix:")
 # print(length(unique(df_merged_z$gene)))
-
-##############################
-# LM22 splice ref vs all matched genes
-##############################
-# Read in gene exp 
-df_exp <- read.table(file= paste0(
-                              opt$geneDir,
-                              "combined_kallisto_log2tpm_batch_corrected.csv"),
-                    sep=",", header = TRUE, row.names=1)
-df_exp <- df_exp %>% mutate_if(is.character,as.numeric)
-print(head(df_exp))
-print(dim(df_exp))
-
-# get medians and z-score across group
-df_exp_LM22_med <- calcGroupMed(df_exp, unlist(unique(df_splice_ref$overlapping)), "LM22")
-df_exp_LM22_med_z <- t(scale(t(df_exp_LM22_med)))
-df_exp_LM6_med <- calcGroupMed(df_exp, unlist(unique(df_splice_ref$overlapping)), "LM6")
-df_exp_LM6_med_z <- t(scale(t(df_exp_LM6_med)))
-
-print(head(df_exp_LM22_med_z))
-print(dim(df_exp_LM22_med_z))
-print("------")
-print(head(df_exp_LM6_med_z))
-print(dim(df_exp_LM6_med_z))
-
-# Combine LM6 and LM22
-df_exp_LM6_LM22_med_z <- cbind(df_exp_LM6_med_z, df_exp_LM22_med_z)
-print("Combined......")
-print(head(df_exp_LM6_LM22_med_z))
-print(dim(df_exp_LM6_LM22_med_z))
-
-# Replace spaces with _ to match other df
-str_conv <- function(in_str){
-  paste(unlist(strsplit(as.character(in_str), split=" ")),
-                                    collapse="_")
-}
-
-# Replace spaces with _ to match other df
-new_names <- unlist(lapply(colnames(df_exp_LM6_LM22_med_z),FUN= str_conv))
-colnames(df_exp_LM6_LM22_med_z) <- new_names
-print(head(df_exp_LM6_LM22_med_z))
-
-# Check new columns str format matches ref mat rows
-# Add stop if not equal 
-print(dim(df_splice_ref))
-print(dim(df_splice_ref %>% filter(cell_type %in% colnames(df_exp_LM6_LM22_med_z))))
-
-# Go through each splice event and get row for that gene 
-print(head(df_splice_ref))
-
-# # All genes that were in splice ref matrix
-df_splice_ref_genes <- unique(df_splice_ref$overlapping)
-print(length(df_splice_ref_genes))
-print("=======================================================================")
-
-df_splice_ref['gene_z'] <- NA
-df_splice_ref['splice_z'] <- NA
-print(head(df_splice_ref))
-# quit()
-# Adding gene z_scores to splice ref df
-# for (gene in head(df_splice_ref_genes)){
-
-for (gene in df_splice_ref_genes){
-  print("start.............")
-  print(gene)
-  #  For each gene get the cell_types the splice event was signifcant in
-  df_  <- df_splice_ref %>% filter(overlapping == gene)
-  # print(as.vector(unlist(unique(df_$cell_type))))
-
-  # Check if gene in gene exp data (splicing can have lists for overlaps "AC129492.1,AC129492.4")
-  if (gene %in% rownames(df_exp_LM6_LM22_med_z)){
-    
-    # Iterate over cell types
-    for (gene_cell in as.vector(unlist(unique(df_$cell_type)))){
-      # print(gene_cell)
-
-      # Look up the gene exp val for gene and cell type
-      z <- df_exp_LM6_LM22_med_z[gene, gene_cell] 
-      # print(z)
-
-      # Add gene z to each corresponding splice ref rows
-      df_splice_ref <- df_splice_ref %>%
-        mutate(gene_z = ifelse(((overlapping==gene) & (cell_type==gene_cell)), z, gene_z))
-    }
-  } else
-  {print("no gene match")}
-}
-
-
-print(head(df_splice_ref))
-# print(df_splice_ref)
-
-# for (event in df_splice_ref_genes$event){
-
-
-
-
-# }
-
-
-# Adding splice z_scores to splice ref df
-
-
-# event   cell type gene_zscore splice_zscore 
-
-
-
-	# - for each reference matrix splice junction
-	# 	- for that cell type pull the gene expression from the big table
-	# 	- z-score all
-	# 	- scatter plot one labeling gene
-	# 	- another labeling event
-	# 	- another labeling cell-type sig
-	# 	- only plot the z-score for the cell_type specific one 
 
