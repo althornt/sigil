983a31f3e65b076f2a361a3341ec7689ccf058a7
althornt <alxs412@gmail.com>
2022-05-31 04:22:44 +0000

    add accuracy calc and plot

--- bin/benchmarkSplicingSets.R
+++ bin/benchmarkSplicingSets.R
@@ -21,19 +21,151 @@ library(tidyr)
 library(reshape2)
 library(ComplexHeatmap)
 
-
 ##########################
 # Functions
 ##########################
-save_pheatmap_pdf <- function(x, filename, width=7, height=7) {
-  #' Function to save pheatmaps to a pdf file
-  stopifnot(!missing(x))
-  stopifnot(!missing(filename))
-  pdf(filename, width=width, height=height)
-  grid::grid.newpage()
-  grid::grid.draw(x$gtable)
+ssgsea_heatmap <- function(dat.gct, out_path){
+# colors from https://sashamaps.net/docs/resources/20-colors/
+
+  ls_col = c(
+      "B cells memory" = "#469990", #Teal                           
+      "B cells naive"  = "#000075", #Navy
+
+      "Eosinophils" = "#42d4f4",
+      "Neutrophils" = "#4363d8",
+
+      "Monocytes" = "#800000", #Black
+      "Macrophages M0" = "#9A6324",           
+      "Macrophages M1"  = "#808000", #Maroon
+
+      "Dendritic cells resting" = "#f58231",
+      "Dendritic cells activated" = "#e6194B",
+                          
+      "NK cells resting" = "#ffe119",
+
+      "T cells gamma delta"   = "#fabed4", #pink
+      "T cells follicular helper" = "#ffd8b1", #apricot
+      "T cells regulatory"  = "#fffac8", #beige
+      "T cells CD4 naive"   = "#aaffc3", #mint
+      "T cells CD8"         = "#dcbeff" #lavender
+  )
+
+  ha <- HeatmapAnnotation(
+      df = metadata, 
+      name = "main_label", 
+      col = list("main_label"= ls_col, "data_source" = c("Choi" = "black",
+                                                       "Monaco" = "darkgrey" , 
+                                                       "Song" = "lightgrey")),
+      #  na_col = "grey",
+      # annotation_legend_param = list(),
+      show_legend = TRUE,
+      which = "column",
+      gp = gpar(col = NA)
+      # border = FALSE,
+      # gap = unit(1, "points"),
+
+      # show_annotation_name = TRUE,
+      # annotation_name_gp = gpar(),
+      # annotation_name_offset = NULL,
+      # annotation_name_side = ifelse(which == "column", "right", "bottom"),
+      # annotation_name_rot = NULL,
+
+      # annotation_height = NULL,
+      # annotation_width = NULL,
+      # height = NULL,
+      # width = NULL,
+      # simple_anno_size = ht_opt$simple_anno_size,
+      # simple_anno_size_adjust = FALSE
+      )
+
+  ###############################################################################
+  # clustered unscaled heatmap 
+  png(file=paste0(out_path,"/heatmap_unscaled_clustered.png"),
+      width = 9,
+      height    = 10,
+      units     = "in",
+      res       = 1200)
+
+  ht <- ComplexHeatmap::Heatmap(dat.gct,
+                                name="Z-Score",
+                                  show_row_names= TRUE,
+                                  show_column_names = FALSE,
+                                  row_names_gp = grid::gpar(fontsize =5),
+                                  top_annotation=ha,
+                                  # heatmap_legend_param = list(legend_gp = gpar(fontsize = 3)),
+                                  show_row_dend = FALSE,
+                                  heatmap_legend_param = list(
+                                  # legend_direction = "horizontal", 
+                                  # legend_height = unit(1, "cm"),
+                                  legend_gp = gpar(fontsize = 5))
+                )
+
+  draw(ht, merge_legend = TRUE)
   dev.off()
+
+  ##############################################################################
+  # clustered scaled heatmap 
+  dat.gct <- t(scale(t(dat.gct))) # scale and center rows
+
+  png(file=paste0(out_path,"/heatmap_scaled_clustered.png"),
+      width = 9,
+      height    = 10,
+      units     = "in",
+      res       = 1200)
+
+  ht_scaled_clustered <- ComplexHeatmap::Heatmap(dat.gct,
+                                name="Z-Score",
+                                  show_row_names= TRUE,
+                                  show_column_names = FALSE,
+                                  row_names_gp = grid::gpar(fontsize =5),
+                                  top_annotation=ha,
+                                  # heatmap_legend_param = list(legend_gp = gpar(fontsize = 3)),
+                                  show_row_dend = FALSE,
+                                  heatmap_legend_param = list(
+                                  # legend_direction = "horizontal", 
+                                  # legend_height = unit(1, "cm"),
+                                  legend_gp = gpar(fontsize = 5))
+                )
+
+  draw(ht_scaled_clustered, merge_legend = TRUE)
+  dev.off()
+
+  ##############################################################################
+  # ordered scaled heatmap 
+  ls_sample_order <- metadata %>% 
+    rownames_to_column("Run") %>%
+    arrange(main_label) %>%
+    pull(Run)
+
+  png(file=paste0(out_path,"/heatmap_scaled_ordered.png"),
+      width = 9,
+      height    = 10,
+      units     = "in",
+      res       = 1200)
+
+  ht_order <- ComplexHeatmap::Heatmap(dat.gct,
+                                name="Z-Score",
+                                  column_order =ls_sample_order,
+                                  row_order = order(rownames(dat.gct)), 
+                                  show_row_names= TRUE,
+                                  show_column_names = FALSE,
+                                  row_names_gp = grid::gpar(fontsize =5),
+                                  top_annotation=ha,
+                                  # heatmap_legend_param = list(legend_gp = gpar(fontsize = 3)),
+                                  show_row_dend = FALSE,
+                                  heatmap_legend_param = list(
+                                  # legend_direction = "horizontal", 
+                                  # legend_height = unit(1, "cm"),
+                                  legend_gp = gpar(fontsize = 5))
+                )
+
+  draw(ht_order, merge_legend = TRUE)
+  dev.off()
+
 }
+
+
+
 # Arguments
 option_list <- list(
   optparse::make_option(
@@ -61,6 +193,7 @@ opt <- optparse::parse_args(opt_parser)
 # Open files
 metadata <- read.csv(file = opt$metadata)
 df_sample_annotations <- metadata %>%
+  arrange(Run) %>%
   dplyr::select(Run,main_label,group_label, sigil_general, data_source) %>%
   tibble::column_to_rownames("Run") %>%
   t()
@@ -83,84 +216,91 @@ gct_in <- "/mnt_/benchmark/all_data.gct"
 dat.gct <- read.delim(file=gct_in, skip=2) %>%
   select(-Description) %>%
   column_to_rownames("Name") 
-
-dat.gct <- t(scale(t(dat.gct))) # scale and center rows
+dat.gct <- dat.gct[ , order(names(dat.gct))]
 
 metadata <- metadata %>% 
+    arrange(Run) %>%
     tibble::column_to_rownames("Run") %>%
-    select(main_label)
-
-# https://sashamaps.net/docs/resources/20-colors/
-
-ls_col = c(
-    "B cells memory" = "#469990", #Teal                           
-    "B cells naive"  = "#000075", #Navy
-
-    "Eosinophils" = "#42d4f4",
-    "Neutrophils" = "#4363d8",
-
-    "Monocytes" = "#800000", #Black
-    "Macrophages M0" = "#9A6324",           
-    "Macrophages M1"  = "#808000", #Maroon
-
-    "Dendritic cells resting" = "#f58231",
-    "Dendritic cells activated" = "#e6194B",
-                        
-    "NK cells resting" = "#ffe119",
-
-    "T cells gamma delta"   = "#fabed4", #pink
-    "T cells follicular helper" = "#ffd8b1", #apricot
-    "T cells regulatory"  = "#fffac8", #beige
-    "T cells CD4 naive"   = "#aaffc3", #mint
-    "T cells CD8"         = "#dcbeff" #lavender
-)
+    select(main_label, data_source)
 
 
-png(file=paste0(opt$out_dir,"/heatmap.png"),
-    width = 9,
-    height    = 10,
-    units     = "in",
-    res       = 1200)
+# Make heatmap of ssGSEA res
+ssgsea_heatmap(dat.gct, opt$out_dir)
 
-ha <- HeatmapAnnotation(
-    df = metadata, 
-    name = "main_label", 
-    col = list("main_label"= ls_col),
-    #  na_col = "grey",
-    # annotation_legend_param = list(),
-    show_legend = TRUE,
-    which = "column",
-    gp = gpar(col = NA)
-    # border = FALSE,
-    # gap = unit(1, "points"),
+dat.gct_scale <- t(scale(t(dat.gct))) # scale and center rows
 
-    # show_annotation_name = TRUE,
-    # annotation_name_gp = gpar(),
-    # annotation_name_offset = NULL,
-    # annotation_name_side = ifelse(which == "column", "right", "bottom"),
-    # annotation_name_rot = NULL,
+# Drop gene sets that arent UP or DN
+dat.gct_UPDN <- dat.gct_scale %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("Run") %>%
+    dplyr::filter( grepl('UP|DN', Run)) %>%
+    tibble::column_to_rownames("Run") 
 
-    # annotation_height = NULL,
-    # annotation_width = NULL,
-    # height = NULL,
-    # width = NULL,
-    # simple_anno_size = ht_opt$simple_anno_size,
-    # simple_anno_size_adjust = FALSE
-    )
+# Add row for which set has max and min score in each sample 
+max <- rownames(dat.gct_UPDN)[apply(dat.gct_UPDN,2,which.max)]
+dat.gct_UPDN["max",] <- max
+min <- rownames(dat.gct_UPDN)[apply(dat.gct_UPDN,2,which.min)]
+dat.gct_UPDN["min",] <- min
 
-ht <- ComplexHeatmap::Heatmap(dat.gct,
-                              name="Z-Score",
-                                show_row_names= TRUE,
-                                show_column_names = FALSE,
-                                row_names_gp = grid::gpar(fontsize =5),
-                                top_annotation=ha,
-                                # heatmap_legend_param = list(legend_gp = gpar(fontsize = 3)),
-                                show_row_dend = FALSE,
-                                heatmap_legend_param = list(
-                                # legend_direction = "horizontal", 
-                                # legend_height = unit(1, "cm"),
-                                legend_gp = gpar(fontsize = 5))
-               )
+dat.gct_UPDN["main_label", ] <- df_sample_annotations["main_label",]
 
-draw(ht)
-dev.off()
+tail(dat.gct_UPDN)
+print("-----------------------")
+
+# Transpose to compare label to max and min 
+# check if max value is label + "UP"
+# check if min value is label + "DN"
+df_ <- dat.gct_UPDN %>%
+    t() %>%
+    as.data.frame() %>%
+    select(main_label, min, max) %>%
+    mutate(main_label_str = gsub(" ", "_", main_label)) %>%
+    mutate(max_match= ifelse((startsWith(as.character(max),
+                                        as.character(main_label_str)) & 
+                                endsWith(as.character(max),
+                                        as.character("UP"))) , 1, 0)) %>%
+    mutate( min_match= ifelse((startsWith(as.character(min),
+                                        as.character(main_label_str)) & 
+                                endsWith(as.character(min),
+                                        as.character("DN"))) , 1, 0)) 
+    
+print(df_)
+
+
+print("min")
+print(sum(df_$min_match)/nrow(df_))
+print("max")
+print(sum(df_$max_match)/nrow(df_))
+
+write.csv(df_, paste0(opt$out_dir, "accuracy.csv"))
+
+
+# group by label 
+df_min_match <- df_ %>%
+  group_by(main_label) %>%
+  summarise_at(vars(min_match), list(perc_min_match = mean)) %>%
+  arrange(desc(perc_min_match)) %>%
+  tibble::column_to_rownames("main_label")
+
+print(df_min_match)
+
+df_max_match <- df_ %>%
+  group_by(main_label) %>%
+  summarise_at(vars(max_match), list(perc_max_match = mean)) %>%
+  arrange(desc(perc_max_match)) %>%
+  tibble::column_to_rownames("main_label")
+
+print(df_max_match)
+
+df_matches <- cbind(df_min_match, df_max_match)
+print(df_matches)
+
+df_matches_ <- df_matches %>%
+  tibble::rownames_to_column("label") %>%
+  gather("type", "percent", -label)
+print(df_matches_)
+
+s <- ggplot(df_matches_, aes(y =percent, x= label, fill=type) )+
+      geom_bar(stat = "identity", position = "dodge") +
+     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave(plot=s, filename=paste0(opt$out_dir, "plot.png"))
